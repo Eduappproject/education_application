421bd1739242ea29323ae8fa1c90b791e412341c
wwd2022 <wwd_@naver.com>
2022-06-09 18:29:41 +0900

    회원가입,이메일 인증,회원가입시 서버로 보내는 메시지 구현(일부 누락)

--- client.py
+++ client.py
@@ -1,18 +1,35 @@
 import sys
+import time
+import random
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
 from socket import *
+from email.mime.text import MIMEText  # 이메일 전송을 위한 라이브러리 import
+import smtplib
+import re  # 정규 표현식
 
 form_class = uic.loadUiType("untitled.ui")[0]
 
 
 # 클라이언트 스레드
 class ClientWorker(QThread):
+    client_data_emit = pyqtSignal(str)
 
     def run(self):
-        pass
+        while True:
+            try:
+                msg = self.sock.recv(1024).decode()
+                print(msg)
+                if not msg:
+                    print("연결 종료(메시지 없음)")
+                    break
+            except:
+                print("연결 종료(예외 처리)")
+                break
+            else:
+                self.client_data_emit.emit(f"{msg}")
         # 서버에서 오는 값을 받을 준비 해야한다
 
 
@@ -40,19 +57,32 @@ class AcceptWorker(QThread):
                 # 생성에 실패(오류)하면 반복문 멈추지 않음
             i += 1
             if i >= 100:
-                input("\n\n서버를 열수 없습니다.\n엔터키를 누를시 재시도 합니다")
+                input("\n\n서버를 열 수 없습니다.\n엔터키를 누를시 재시도 합니다")
                 i = 0
 
         self.sock.listen(5)
-
         while True:
             # c 에 소켓 객체를 넣고 a 에 주소를 넣는다
             c, a = self.sock.accept()
-            # 접속하자마자 접속한 클라이언트가 자신의 정보를 표현하는 메시지를 서버로 보낸다
-            # 그걸 서버가 받는다
-            msg = c.recv(1024).decode()
-            # 받은 메시지를 화면에 표현한다(디버그 용)
-            self.server_data_emit.emit(f"msg:{msg}")
+            while True:
+                try:
+                    msg = c.recv(1024).decode()
+                    # 받은 메시지를 화면에 표현한다(디버그 용)
+                    self.server_data_emit.emit(f"{msg}")
+                    if msg == "signup":
+                        while True:
+                            id_check = c.recv(1024).decode()
+                            if id_check == "Q_reg":
+                                self.server_data_emit.emit(f"{id_check}")
+                                break
+                            self.server_data_emit.emit(f"{id_check}")
+                            if id_check in ["qqq", "www", "eee"]:
+                                c.send("!NO".encode())
+                            else:
+                                c.send("!OK".encode())
+
+                except:
+                    break
 
 
 # 여기서 서버와 연결할 클라이언트 실행하기(클라이언트 소켓 생성)
@@ -69,8 +99,9 @@ class WindowClass(QMainWindow, form_class):
         self.SignUpPushButton.clicked.connect(self.SignUpPushButton_event)  # 회원가입(페이지로 이동) 버튼
         self.SignUpPushButton_2.clicked.connect(self.SignUpPushButton_2_event)  # 회원가입 버튼
         self.BackButton.clicked.connect(self.BackButton_event)  # 회원가입 취소 버튼
-        # self.EmailCheckPushButton.clicked.connect()  # 이메일 인증요청 버튼
-        # self.EmailCheckNumberPushButton.clicked.connect()  # 이메일에 도착한 인증번호 확인 버튼
+        self.SignUpCheckButton.clicked.connect(self.SignUpCheckButton_event)
+        self.EmailCheckPushButton.clicked.connect(self.EmailCheckPushButton_event)  # 이메일 인증요청 버튼
+        self.EmailCheckNumberPushButton.clicked.connect(self.EmailCheckNumberPushButton_event)  # 이메일에 도착한 인증번호 확인 버튼
         self.beackButton_2.clicked.connect(self.beackButton_2_event)
         # # 메인 화면
         # self.mainButton_1.clicked.connect()  # 등급 버튼
@@ -84,26 +115,22 @@ class WindowClass(QMainWindow, form_class):
         self.lineEdit_new_id.textChanged[str].connect(self.lineEdit_text_changed)
         self.lineEdit_new_pw.textChanged[str].connect(self.lineEdit_text_changed)
         self.lineEdit_new_pw_check.textChanged[str].connect(self.lineEdit_text_changed)
+        self.lineEdit_email.textChanged[str].connect(self.lineEdit_text_changed)
+        # 회원가입화면 라디오버튼
+        self.userCheckButton_1.clicked.connect(self.userCheckButton_1_event)
+        self.userCheckButton_2.clicked.connect(self.userCheckButton_2_event)
 
         # 서버 스레드 선언
         self.T = AcceptWorker()
         self.T.server_data_emit.connect(self.server_log)
         self.T.start()  # 서버 스레드 실행
-
+        time.sleep(1)
         # 메모
         # from pprint import pprint  # 보기 편하게 프린트 해주는 함수
         # pprint(self.__dict__)  # 접근 가능한 객체의 변수를 표시(해당 변수에 접근해 메서드를 쓸수있다
         # self.__dict__['label'].setText("학생용 클라이이언트 로그인 화면")  # 예시
         # # 이를 이용해서 여러개의 이벤트 핸들러를 설정하거나 수십개의 라벨을 원하는 텍스트로 바꿀수 있다
 
-    # 로그인 화면
-    def loginPushButton_event(self):
-        id = self.loginLineEdit.text()
-        pw = self.loginLineEdit_2.text()
-        if id == "":
-            id = "아이디"
-        if pw == "":
-            pw = "비밀번호"
         # 소켓 생성
         self.sock = socket(AF_INET, SOCK_STREAM)
         # 포트 번호 8500 ~ 8599 사이에 서버를 찾는다
@@ -122,27 +149,69 @@ class WindowClass(QMainWindow, form_class):
                 print("서버를 찾을수 없습니다")
                 input("엔터키를 누를시 다시 검색합니다")
                 i = 0
+
+        self.user = ClientWorker()
+        self.user.sock = self.sock
+        self.user.client_data_emit.connect(self.sock_msg)
+        self.user.start()
+
+    # 로그인 화면
+    def loginPushButton_event(self):
+        # id = self.loginLineEdit.text()
+        # pw = self.loginLineEdit_2.text()
+        # if id == "":
+        #     id = "아이디"
+        # if pw == "":
+        #     pw = "비밀번호"
+
         self.sock.send(f"{id},{pw}".encode())
 
         self.stackedWidget.setCurrentIndex(2)
+        self.loginLineEdit.setText("")
+        self.loginLineEdit_2.setText("")
+
+    # 회원가입 페이지로 이동
     def SignUpPushButton_event(self):
+        self.sock.send("signup".encode())  # 서버에게 회원가입 하겠다고 보냄
+        self.loginLineEdit.setText("")
+        self.loginLineEdit_2.setText("")
         self.stackedWidget.setCurrentIndex(1)
 
     # 회원가입 화면
     def lineEdit_text_changed(self):  # 이름과 비밀번호를 입력할때마다 실행됨
+        # 아이디 중복확인 버튼 활성화 여부
+
         # text() 메서드는 lineEdit 에 입력된 글자를 가져옵니다fewfw
-        # 셋중하나라도 문자가 없다면(Faise) 문자를 입력해달라고합니다
+        # 하나라도 문자가 없다면(Faise) 문자를 입력해달라고합니다
         self.SignUpPushButton_2.setEnabled(False)
         label_text = []
         if not self.lineEdit_new_name.text():
             label_text.append("이름를 입력해주세요.")
+
         if not self.lineEdit_new_id.text():
             label_text.append("아이디를 입력해주세요.")
+            self.SignUpCheckButton.setEnabled(False)
+        elif not self.lineEdit_new_id.isEnabled():
+            self.SignUpCheckButton.setEnabled(False)
+        else:
+            self.SignUpCheckButton.setEnabled(True)
+
         if not (self.lineEdit_new_pw.text() or self.lineEdit_new_pw_check.text()):
             label_text.append("비밀번호를 입력해 주세요.")
         elif self.lineEdit_new_pw.text() != self.lineEdit_new_pw_check.text():
             label_text.append("비밀번호가 동일하지 않습니다.")
-        if not label_text:
+        elif not self.lineEdit_email.text():
+            label_text.append("이메일을 입력해주세요.")
+            self.EmailCheckPushButton.setEnabled(False)
+        else:
+            if self.lineEdit_email.isEnabled():
+                self.EmailCheckPushButton.setEnabled(True)
+            else:
+                self.EmailCheckPushButton.setEnabled(False)
+
+        if not (self.lineEdit_email_check.text() and not self.EmailCheckNumberPushButton.isEnabled()):
+            label_text.append("인증번호를 입력해주세요.")
+        if not label_text and not self.lineEdit_new_id.isEnabled():
             # 위에있는 모든 조건을 통과했다면 정상적인 회원가입이 가능하다고 판단하고 회원가입 버튼을 활성화 합니다.
             self.SignUpPushButton_2.setEnabled(True)
             label_text.append("회원가입 버튼을 눌러주세요.")
@@ -150,10 +219,78 @@ class WindowClass(QMainWindow, form_class):
         self.SignUpLabel.adjustSize()  # 라벨에 적힌 글자에맞춰서 라벨 사이즈를 조절해주는 메서드
 
     def SignUpPushButton_2_event(self):  # 회원가입 버튼
-        self.lineEdit_new_name.text()
-        self.lineEdit_new_id.text()
-        self.lineEdit_new_pw_check.text()
+        self.sock.send(
+            f"{self.lineEdit_new_pw_check.text()}"
+            f"/{self.lineEdit_new_name.text()}"
+            f"/{self.lineEdit_email.text()}"
+            f"/{'학생' if self.userCheckButton_1.isChecked() else '교사'}"
+            .encode())
+        self.sign_up_back()
 
+    # 회원가입 창을 닫는 버튼
+    def BackButton_event(self):
+        self.sock.send("Q_reg".encode())
+        self.sign_up_back()
+
+    # 아이디 중복확인 버튼
+    def SignUpCheckButton_event(self):
+        input_id = self.lineEdit_new_id.text()
+        self.sock.send(input_id.encode())
+
+    def userCheckButton_1_event(self):
+        self.userCheckButton_2.setChecked(False)
+        print("self.userCheckButton_1.isChecked()", self.userCheckButton_1.isChecked())
+        print("self.userCheckButton_2.isChecked()", self.userCheckButton_2.isChecked())
+
+    def userCheckButton_2_event(self):
+        self.userCheckButton_1.setChecked(False)
+        print("self.userCheckButton_1.isChecked()", self.userCheckButton_1.isChecked())
+        print("self.userCheckButton_2.isChecked()", self.userCheckButton_2.isChecked())
+
+    def beackButton_2_event(self):
+        self.stackedWidget.setCurrentIndex(0)
+
+    # 이메일 인증 요청 버튼을 눌렀을때
+    # 해당 이메일에 인증번호 보내기
+    # 이메일에 보낸 인증번호를 클라이언트에 저장하고
+    # 인증번호 인증시 클라이언트에 저장된 인증번호와 일치하는지 확인하기
+    def EmailCheckPushButton_event(self):
+        email = self.lineEdit_email.text()
+        re_text = re.compile(".+@\w+\.\w")
+
+        # 이메일 형식이 맞는지 검사합니다
+        if re_text.match(email):
+            print("사용가능한 이메일")
+        else:
+            print("잘못된 이메일")
+            return
+
+        ses = smtplib.SMTP('smtp.gmail.com', 587)  # smtp 세션 설정
+        ses.starttls()
+        # 이메일을 보낼 gmail 계정에 접속
+        ses.login('uihyeon.bookstore@gmail.com', 'ttqe mztd lljo tguh')
+
+        self.check_msg = str(random.randrange(1000, 10000))
+        msg = MIMEText('인증번호: ' + self.check_msg)  # 보낼 메세지 내용을 적는다
+        msg['subject'] = 'PyQt5 에서 인증코드를 발송했습니다.'  # 보낼 이메일의 제목을 적는다
+        # 앞에는 위에서 설정한 계정, 두번째에는 이메일을 보낼 계정을 입력
+        ses.sendmail('PyQt5.ABC@gmail.com', email, msg.as_string())
+
+        self.lineEdit_email.setEnabled(False)
+        self.EmailCheckPushButton.setEnabled(False)
+        self.lineEdit_email_check.setEnabled(True)
+        self.EmailCheckNumberPushButton.setEnabled(True)
+        pass
+
+    # 인증번호 인증 버튼
+    def EmailCheckNumberPushButton_event(self):
+        if self.lineEdit_email_check.text() == self.check_msg:
+            print("인증되었습니다")
+            self.lineEdit_email_check.setEnabled(False)
+            self.EmailCheckNumberPushButton.setEnabled(False)
+            self.SignUpPushButton_2.setEnabled(True)
+
+    def sign_up_back(self):
         self.stackedWidget.setCurrentIndex(0)
         # 회원가입이 끝나고 로그인페이지로 이동하면서 입력창을 빈칸으로 만든다
         self.lineEdit_new_name.setText("")
@@ -162,23 +299,36 @@ class WindowClass(QMainWindow, form_class):
         self.lineEdit_new_pw_check.setText("")
         self.lineEdit_email.setText("")
         self.lineEdit_email_check.setText("")
+        self.lineEdit_new_id.setEnabled(True)
+        self.SignUpCheckButton.setEnabled(True)
+        self.EmailCheckPushButton.setEnabled(False)
+        self.lineEdit_email_check.setEnabled(False)
+        self.lineEdit_email.setEnabled(False)
+        self.EmailCheckNumberPushButton.setEnabled(False)
+        self.SignUpPushButton_2.setEnabled(False)
+        self.check_msg = ""
 
-    def BackButton_event(self):
-        self.stackedWidget.setCurrentIndex(0)
-        # 로그인페이지로 이동하면서 입력창을 빈칸으로 만든다
-        self.lineEdit_new_name.setText("")
-        self.lineEdit_new_id.setText("")
-        self.lineEdit_new_pw.setText("")
-        self.lineEdit_new_pw_check.setText("")
-        self.lineEdit_email.setText("")
-        self.lineEdit_email_check.setText("")
-
+    # 테스트용 서버의 동작읋 확인하기 위해서 만든 함수
     @pyqtSlot(str)
     def server_log(self, data):
         self.logTextBrowser.append(data)
 
-    def beackButton_2_event(self):
-        self.stackedWidget.setCurrentIndex(0)
+    # 클라이언트가 서버로 받은 메시지를 메인스레드 에서 처리하기 위해 만든 함수
+    @pyqtSlot(str)
+    def sock_msg(self, msg):
+        self.logTextBrowser_2.append(msg)
+        if msg == "!OK":
+            page_index = self.stackedWidget.currentIndex()
+            if 1 == page_index:
+                self.lineEdit_new_id.setEnabled(False)
+                self.SignUpCheckButton.setEnabled(False)
+                self.lineEdit_email.setEnabled(True)
+        if msg == "!NO":
+            page_index = self.stackedWidget.currentIndex()
+            if 1 == page_index:
+                self.logTextBrowser_2.append("중복된 아이디가 있습니다")
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = WindowClass()
---
+++ 참고용 서버/server.py
@@ -0,0 +1,297 @@
+import socket
+import threading
+import sqlite3
+import datetime
+from datetime import date
+import sys
+
+PORT = 2090
+BUF_SIZE = 2048
+lock = threading.Lock()
+clnt_imfor = []  # [[소켓, id]]
+clnt_cnt = 0
+
+
+def dbcon():  # db연결
+    con = sqlite3.connect('serverDB.db')  # DB 연결
+    c = con.cursor()  # 커서
+    return (con, c)
+
+
+def handle_clnt(clnt_sock):  # 핸들클라
+    for i in range(0, clnt_cnt):
+        if clnt_imfor[i][0] == clnt_sock:
+            clnt_num = i
+            break  # 접속한 클라 저장
+
+    while True:
+        sys.stdout.flush()  # 버퍼 비워주는거
+        clnt_msg = clnt_sock.recv(BUF_SIZE)
+
+        if not clnt_msg:
+            lock.acquire()  # 뮤텍스같은거
+            delete_imfor(clnt_sock)
+            lock.release()
+            break
+        clnt_msg = clnt_msg.decode()  # 숫자->문자열로 바꾸는거 맞나?  데이터 보낼때 incode 로 하고
+
+        sys.stdin.flush()
+
+        if 'signup' == clnt_msg:
+            sign_up(clnt_sock, clnt_num)
+        elif clnt_msg.startswith('login/'):  # startswitch -->문자열중에 특정 문자를 찾고싶거나, 특정문자로 시작하는 문자열, 특정문자로 끝이나는 문자열 등
+            clnt_msg = clnt_msg.replace('login/', '')  # clnt_msg에서 login/ 자름
+            log_in(clnt_sock, clnt_msg, clnt_num)
+        elif clnt_msg.startswith('find_id/'):
+            clnt_msg = clnt_msg.replace('find_id/', '')
+            find_id(clnt_sock, clnt_msg)
+        elif clnt_msg.startswith('find_pw/'):
+            clnt_msg = clnt_msg.replace('find_pw/', '')
+            find_pw(clnt_sock, clnt_msg)
+        elif clnt_msg.startswith('myinfo'):
+            clnt_msg = clnt_msg.replace('myinfo', '')
+            send_user_information(clnt_num)
+        elif clnt_msg.startswith('edit_data'):
+            clnt_msg = clnt_msg.replace('edit_data', '')
+            edit_data(clnt_num, clnt_msg)
+        elif clnt_msg.startswith('remove'):
+            remove(clnt_num)
+        else:
+            continue
+
+
+def edit_data(clnt_num, clnt_msg):  # 데이터 베이스 정보변경
+    print(clnt_msg)
+    id = clnt_imfor[clnt_num][1]
+    con, c = dbcon()
+    if clnt_msg.startswith('_name/'):
+        clnt_msg = clnt_msg.replace('_name/', '')
+        lock.acquire()
+        c.execute("UPDATE usertbl SET name = ? WHERE id = ?", (clnt_msg, id))
+        con.commit()
+        lock.release()
+        con.close()
+    elif clnt_msg.startswith('_pw/'):
+        clnt_msg = clnt_msg.replace('_pw/', '')
+        lock.acquire()
+        c.execute("UPDATE usertbl SET password = ? WHERE id = ?", (clnt_msg, id))
+        con.commit()
+        lock.release()
+        con.close()
+    else:
+        con.close()
+        return
+
+
+def sign_up(clnt_sock, clnt_num):  # 회원가입
+    con, c = dbcon()
+    check = 0
+    user_data = []
+
+    while True:
+        check = 0
+        imfor = clnt_sock.recv(BUF_SIZE)
+        imfor = imfor.decode()
+        if imfor == "Q_reg":  # 회원가입 창 닫을 때 함수 종료
+            con.close()
+            break
+        c.execute("SELECT userid FROM usertbl")  # usertbl 테이블에서 id 컬럼 추출
+        for row in c:  # id 컬럼
+            if imfor in row:  # 클라이언트가 입력한 id가 DB에 있으면 (중복 제거)
+                clnt_sock.send('!NO'.encode())
+                check = 1
+                break
+        if check == 1:
+            continue
+
+        clnt_sock.send('!OK'.encode())  # 중복된 id 없으면 !OK 전송
+
+        lock.acquire()
+        user_data.append(imfor)  # user_data에 id 추가
+        imfor = clnt_sock.recv(BUF_SIZE)  # password/name/email/usertype
+        imfor = imfor.decode()
+        if imfor == "Q_reg":  # 회원가입 창 닫을 때 함수 종료
+            con.close()
+            break
+
+        imfor = imfor.split('/')  # 구분자 /로 잘라서 리스트 생성
+        for imfo in imfor:
+            user_data.append(imfo)  # user_data 리스트에 추가
+        query = "INSERT INTO usertbl(userid, userpw, username, email, usertype) VALUES(?, ?, ?, ?, ?)"
+
+        c.executemany(query, (user_data,))  # DB에 user_data 추가
+        con.commit()  # DB에 커밋
+        con.close()
+        lock.release()
+        break
+
+
+def log_in(clnt_sock, data, clnt_num):  # 로그인
+    con, c = dbcon()
+    data = data.split('/')
+    user_id = data[0]
+
+    c.execute("SELECT userpw FROM usertbl where userid=?",
+              (user_id,))  # DB에서 id 같은 password 컬럼 선택
+
+    user_pw = c.fetchone()  # 한 행 추출
+
+    if not user_pw:  # DB에 없는 id 입력시
+        clnt_sock.send('iderror'.encode())
+        con.close()
+        return
+
+    if (data[1],) == user_pw:
+        # 로그인성공 시그널
+        print("login sucess")
+        clnt_imfor[clnt_num].append(data[0])
+        # c.execute("SELECT book1, book2, book3 FROM usertbl where userid=?", (user_id,))
+        books = c.fetchone()  # 이거 2개 필요없어보인다?
+        books = list(books)  # 이거 2개 필요없어보인다?
+        send_user_information(clnt_num)
+    else:
+        # 로그인실패 시그널
+        clnt_sock.send('!NO'.encode())
+        print("login failure")
+
+    con.close()
+    return
+
+
+def remove(clnt_num):  # 회원탈퇴
+    con, c = dbcon()
+    id = clnt_imfor[clnt_num][1]
+    lock.acquire()
+    c.execute("DELETE FROM usertbl WHERE id = ?", (id,))
+    c.execute("DELETE FROM Return WHERE id = ?", (id,))
+    clnt_imfor[clnt_num].remove(id)
+    con.commit()
+    lock.release()
+    con.close()
+
+
+def send_user_information(clnt_num):  # 유저정보 보낸데
+    con, c = dbcon()
+    id = clnt_imfor[clnt_num][1]
+    clnt_sock = clnt_imfor[clnt_num][0]
+    books = []
+
+    c.execute(
+        "SELECT name FROM usertbl where id=?", (id,))  # 이름, 대여한 책 찾기
+    row = c.fetchone()
+    row = list(row)
+    for i in range(0, len(row)):  # None인 항목 찾기
+        if row[i] == None:
+            row[i] = 'X'
+
+    c.execute("SELECT book_name FROM Return where id=?", (id,))  # 반납한 책
+    while 1:
+        book = c.fetchone()  # 반납한 책 한 권씩 찾기
+        if book is None:
+            break
+        book = list(book)  # 리스트로 변환
+        books = books + book  # books 리스트에 추가
+
+    user_data = row + books  # 이름,대여한 책 + 반납한 책
+    user_data = '/'.join(user_data)
+    # 버퍼 비우기
+
+    clnt_sock.send(('!OK/' + user_data).encode())
+    con.close()
+
+
+def find_id(clnt_sock, email):  # 아이디찾기
+    con, c = dbcon()
+
+    c.execute("SELECT userid FROM usertbl where email=?",
+              (email,))  # DB에 있는 email과 일치시 id 가져오기
+    id = c.fetchone()
+
+    if id == None:  # DB에 없는 email이면 None이므로 !NO 전송
+        clnt_sock.send('!NO'.encode())
+        print('fail')
+        con.close()
+        return
+    else:
+        clnt_sock.send('!OK'.encode())
+        msg = clnt_sock.recv(BUF_SIZE)
+        msg = msg.decode()
+        if msg == "Q_id_Find":  # Q_id_Find 전송받으면 find_id 함수 종료
+            pass
+        elif msg == 'plz_id':  # plz_id 전송받으면 id 전송
+            id = ''.join(id)  # ''<- 여기에는 구분자임 ㅇㅇ  리스트->문자열로 바꾸기
+            clnt_sock.send(id.encode())
+            print('send_id')
+        con.close()
+        return
+
+
+def find_pw(clnt_sock, id):  # 비번찾기
+    con, c = dbcon()
+    c.execute("SELECT userpw, email FROM usertbl where id=?",
+              (id,))  # DB에 있는 id와 일치하면 비밀번호, 이메일 정보 가져오기
+    row = c.fetchone()
+    print(row)
+    if row == None:  # DB에 없는 id면 None
+        clnt_sock.send('!NO'.encode())
+        print('iderror')
+        con.close()
+        return
+
+    clnt_sock.send('!OK'.encode())  # DB에 id 있으면 !OK 전송
+    email = clnt_sock.recv(BUF_SIZE)
+    email = email.decode()
+    if email == "Q_pw_Find":  # Q_pw_Find 전송받으면 find_pw 함수 종료
+        con.close()
+        return
+
+    if row[1] == email:  # 전송받은 email변수 값이 DB에 있는 email과 같으면
+        clnt_sock.send('!OK'.encode())
+        msg = clnt_sock.recv(BUF_SIZE)
+        msg = msg.decode()
+        if msg == "Q_pw_Find":
+            pass
+        elif msg == 'plz_pw':  # plz_pw 전송받으면
+            pw = ''.join(row[0])  # 비밀번호 문자열로 변환
+            clnt_sock.send(pw.encode())
+            print('send_pw')
+        else:
+            pass
+    else:
+        clnt_sock.send('!NO'.encode())
+        print('emailerror')
+
+    con.close()
+    return
+
+
+def delete_imfor(clnt_sock):  # 유저정보 삭제
+    global clnt_cnt
+    for i in range(0, clnt_cnt):
+        if clnt_sock == clnt_imfor[i][0]:
+            print('exit client')
+            while i < clnt_cnt - 1:
+                clnt_imfor[i] = clnt_imfor[i + 1]
+                i += 1
+            break
+    clnt_cnt -= 1
+
+
+# def recv_clnt_msg(clnt_sock):
+
+
+if __name__ == '__main__':  # 메인? 기본설정같은 칸지
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', PORT))
+    sock.listen(5)
+
+    while True:
+        clnt_sock, addr = sock.accept()
+
+        lock.acquire()
+        clnt_imfor.insert(clnt_cnt, [clnt_sock])
+        clnt_cnt += 1
+        print(clnt_sock)
+        lock.release()
+        t = threading.Thread(target=handle_clnt, args=(clnt_sock,))
+        t.start()
