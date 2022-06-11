3da5821178ee505467bd8baf1e023c03767d58ef
PARK <wsx0651@gmail.com>
2022-06-11 15:51:10 +0900

    DB에 유저정보 들어옴

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ PORT = 2090
 BUF_SIZE = 2048
 lock = threading.Lock()
 clnt_imfor = []  # [[소켓, id]]
-clnt_cnt = 0
 
 
 def dbcon(): #db연결
@@ -19,9 +18,9 @@ def dbcon(): #db연결
 
 
 def handle_clnt(clnt_sock): #핸들클라
-    for i in range(0, clnt_cnt):
-        if clnt_imfor[i][0] == clnt_sock:
-            clnt_num = i
+    for clnt_imfo in clnt_imfor:
+        if clnt_imfo[0] == clnt_sock:
+            clnt_num = clnt_imfor.index(clnt_imfo)
             break  # 접속한 클라 저장
 
     while True:
@@ -38,7 +37,7 @@ def handle_clnt(clnt_sock): #핸들클라
         sys.stdin.flush()
 
         if 'signup' == clnt_msg:
-            sign_up(clnt_sock, clnt_num)
+            sign_up(clnt_sock)
         elif clnt_msg.startswith('login/'):  # startswitch -->문자열중에 특정 문자를 찾고싶거나, 특정문자로 시작하는 문자열, 특정문자로 끝이나는 문자열 등
             clnt_msg = clnt_msg.replace('login/', '')  # clnt_msg에서 login/ 자름
             log_in(clnt_sock, clnt_msg, clnt_num)
@@ -67,14 +66,14 @@ def edit_data(clnt_num, clnt_msg): #데이터 베이스 정보변경
     if clnt_msg.startswith('_name/'):
         clnt_msg = clnt_msg.replace('_name/', '')
         lock.acquire()
-        c.execute("UPDATE usertbl SET name = ? WHERE id = ?", (clnt_msg, id))
+        c.execute("UPDATE usertbl SET username = ? WHERE userid = ?", (clnt_msg, id))
         con.commit()
         lock.release()
         con.close()
     elif clnt_msg.startswith('_pw/'):
         clnt_msg = clnt_msg.replace('_pw/', '')
         lock.acquire()
-        c.execute("UPDATE usertbl SET password = ? WHERE id = ?", (clnt_msg, id))
+        c.execute("UPDATE usertbl SET userpw = ? WHERE userid = ?", (clnt_msg, id))
         con.commit()
         lock.release()
         con.close()
@@ -83,26 +82,23 @@ def edit_data(clnt_num, clnt_msg): #데이터 베이스 정보변경
         return
 
 
-def sign_up(clnt_sock, clnt_num): #회원가입
+def sign_up(clnt_sock): #회원가입
     con, c = dbcon()
-    check = 0
     user_data = []
 
     while True:
-        check = 0
         imfor = clnt_sock.recv(BUF_SIZE)
         imfor = imfor.decode()
         if imfor == "Q_reg":      # 회원가입 창 닫을 때 함수 종료
             con.close()
             break
-        c.execute("SELECT userid FROM usertbl")  # usertbl 테이블에서 id 컬럼 추출
-        for row in c:  # id 컬럼
-            if imfor in row:       # 클라이언트가 입력한 id가 DB에 있으면 (중복 제거)
-                clnt_sock.send('!NO'.encode())
-                check = 1
-                break
-        if check == 1:
-            continue
+        c.execute("SELECT userid FROM usertbl where userid = ?", (imfor, ))  # usertbl 테이블에서 id 컬럼 추출
+        row = c.fetchone()
+        if row != None:                      # DB에 없는 id면 None
+            clnt_sock.send('!NO'.encode())
+            print('id_overlap')
+            con.close()
+            return
 
         clnt_sock.send('!OK'.encode())  # 중복된 id 없으면 !OK 전송
 
@@ -113,10 +109,11 @@ def sign_up(clnt_sock, clnt_num): #회원가입
         if imfor == "Q_reg":  # 회원가입 창 닫을 때 함수 종료
             con.close()
             break
-
+        print(imfor)
         imfor = imfor.split('/')  # 구분자 /로 잘라서 리스트 생성
         for imfo in imfor:
             user_data.append(imfo)       # user_data 리스트에 추가
+        print(user_data)
         query = "INSERT INTO usertbl(userid, userpw, username, email, usertype) VALUES(?, ?, ?, ?, ?)"
 
         c.executemany(query, (user_data,))  # DB에 user_data 추가
@@ -133,7 +130,6 @@ def log_in(clnt_sock, data, clnt_num): # 로그인
 
     c.execute("SELECT userpw FROM usertbl where userid=?",
               (user_id,))  # DB에서 id 같은 password 컬럼 선택
-
     user_pw = c.fetchone()             # 한 행 추출
 
     if not user_pw:  # DB에 없는 id 입력시
@@ -145,9 +141,6 @@ def log_in(clnt_sock, data, clnt_num): # 로그인
         # 로그인성공 시그널
         print("login sucess")
         clnt_imfor[clnt_num].append(data[0])
-        #c.execute("SELECT book1, book2, book3 FROM usertbl where userid=?", (user_id,))
-        books = c.fetchone() #이거 2개 필요없어보인다?
-        books = list(books)  #이거 2개 필요없어보인다?
         send_user_information(clnt_num)
     else:
         # 로그인실패 시그널
@@ -162,8 +155,8 @@ def remove(clnt_num): # 회원탈퇴
     con, c = dbcon()
     id = clnt_imfor[clnt_num][1]
     lock.acquire()
-    c.execute("DELETE FROM usertbl WHERE id = ?", (id,))
-    c.execute("DELETE FROM Return WHERE id = ?", (id,))
+    c.execute("DELETE FROM usertbl WHERE userid = ?", (id,))
+    c.execute("DELETE FROM Return WHERE userid = ?", (id,))
     clnt_imfor[clnt_num].remove(id)
     con.commit()
     lock.release()
@@ -174,25 +167,16 @@ def send_user_information(clnt_num):  # 유저정보 보낸데
     con, c = dbcon()
     id = clnt_imfor[clnt_num][1]
     clnt_sock = clnt_imfor[clnt_num][0]
-    books = []
 
     c.execute(
-        "SELECT name FROM usertbl where id=?", (id,))  # 이름, 대여한 책 찾기
+        "SELECT username FROM usertbl where id=?", (id,))  # 이름
     row = c.fetchone()
     row = list(row)
     for i in range(0, len(row)):     # None인 항목 찾기
         if row[i] == None:
             row[i] = 'X'
 
-    c.execute("SELECT book_name FROM Return where id=?", (id,))  # 반납한 책
-    while 1:
-        book = c.fetchone()        # 반납한 책 한 권씩 찾기
-        if book is None:
-            break
-        book = list(book)         # 리스트로 변환
-        books = books + book      # books 리스트에 추가
-
-    user_data = row + books  # 이름,대여한 책 + 반납한 책
+    user_data = row  # 이름
     user_data = '/'.join(user_data)
     # 버퍼 비우기
 
@@ -229,7 +213,7 @@ def find_id(clnt_sock, email):  # 아이디찾기
 
 def find_pw(clnt_sock, id):  #비번찾기
     con, c = dbcon()
-    c.execute("SELECT userpw, email FROM usertbl where id=?",
+    c.execute("SELECT userpw, email FROM usertbl where userid=?",
               (id,))    # DB에 있는 id와 일치하면 비밀번호, 이메일 정보 가져오기
     row = c.fetchone()
     print(row)
@@ -265,19 +249,25 @@ def find_pw(clnt_sock, id):  #비번찾기
     con.close()
     return
 
+def recv_clnt_msg(clnt_sock):
+    sys.stdout.flush()  # 버퍼 비우기
+    clnt_msg = clnt_sock.recv(BUF_SIZE)  # 메세지 받아오기
+    clnt_msg = clnt_msg.decode()  # 디코딩
+    return clnt_msg
+
+
+def send_clnt_msg(clnt_sock, msg):
+    sys.stdin.flush()  # 버퍼 비우기
+    msg = msg.encode()  # 인코딩
+    clnt_sock.send(msg)  # 메세지 보내기
 
 def delete_imfor(clnt_sock): #유저정보 삭제
     global clnt_cnt
-    for i in range(0, clnt_cnt):
-        if clnt_sock == clnt_imfor[i][0]:
+    for clnt_imfo in clnt_imfor:
+        if clnt_sock == clnt_imfo[0]:
             print('exit client')
-            while i < clnt_cnt - 1:
-                clnt_imfor[i] = clnt_imfor[i + 1]
-                i += 1
-            break
-    clnt_cnt -= 1
-
-#def recv_clnt_msg(clnt_sock):
+            index = clnt_imfor.index(clnt_imfo)
+            del clnt_imfor[index]
 
 
 if __name__ == '__main__': #메인? 기본설정같은 칸지
@@ -289,8 +279,7 @@ if __name__ == '__main__': #메인? 기본설정같은 칸지
         clnt_sock, addr = sock.accept()
 
         lock.acquire()
-        clnt_imfor.insert(clnt_cnt, [clnt_sock])
-        clnt_cnt += 1
+        clnt_imfor.append([clnt_sock])
         print(clnt_sock)
         lock.release()
         t = threading.Thread(target=handle_clnt, args=(clnt_sock,))
